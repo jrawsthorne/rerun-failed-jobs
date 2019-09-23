d22e7d3a6b90e0d8002404af71415446d0089278
mihirkamdarcouchbase <mihir.kamdar@couchbase.com>
2019-09-22 21:39:39 -0700

    Update rerun_failed_jobs.py
    Do not rerun TAF jobs

--- rerun_failed_jobs.py
+++ rerun_failed_jobs.py
@@ -207,33 +207,52 @@ class RerunFailedJobs:
         """
         jobs_to_rerun = []
         all_results = []
+        #query = "select `build`, name,component,failCount,totalCount,build_id,url||tostring(build_id) as full_url, \
+        #        'job failed' as reason from {0} where `build`='{1}'\
+        #        and lower(os)='centos' and result='FAILURE' and ( url like '%test_suite_executor-jython/%' or url like \
+        #        '%test_suite_executor-TAF/%' or url like '%test_suite_executor/%') \
+        #        and name not like 'centos-rqg%' order by name;".format(GREENBOARD_DB_BUCKETNAME, current_build_num)
         query = "select `build`, name,component,failCount,totalCount,build_id,url||tostring(build_id) as full_url, \
                 'job failed' as reason from {0} where `build`='{1}'\
-                and lower(os)='centos' and result='FAILURE' and ( url like '%test_suite_executor-jython/%' or url like \
-                '%test_suite_executor-TAF/%' or url like '%test_suite_executor/%') \
+                and lower(os)='centos' and result='FAILURE' and ( url like '%test_suite_executor-jython/%' or url like '%test_suite_executor/%') \
                 and name not like 'centos-rqg%' order by name;".format(GREENBOARD_DB_BUCKETNAME, current_build_num)
+
 
         logger.info("Running query : %s" % query)
         results = self.green_board_bucket.run_query(query)
         all_results.extend(results)
 
+        #query = "select `build`,name,component,failCount,totalCount,build_id,url||tostring(build_id) as full_url, \
+        #        '0 tests passed' as reason from {0} where `build`='{1}'\
+        #        and lower(os)='centos' and result in ['UNSTABLE','ABORTED'] and failCount=totalCount\
+        #        and name not like 'centos-rqg%' and ( url like '%test_suite_executor-jython/%' or url like '%test_suite_executor-TAF/%' or url like '%test_suite_executor/%') order by name;".format(
+        #    GREENBOARD_DB_BUCKETNAME, current_build_num)
         query = "select `build`,name,component,failCount,totalCount,build_id,url||tostring(build_id) as full_url, \
                 '0 tests passed' as reason from {0} where `build`='{1}'\
                 and lower(os)='centos' and result in ['UNSTABLE','ABORTED'] and failCount=totalCount\
-                and name not like 'centos-rqg%' and ( url like '%test_suite_executor-jython/%' or url like '%test_suite_executor-TAF/%' or url like '%test_suite_executor/%') order by name;".format(
+                and name not like 'centos-rqg%' and ( url like '%test_suite_executor-jython/%' or url like '%test_suite_executor/%') order by name;".format(
             GREENBOARD_DB_BUCKETNAME, current_build_num)
         logger.info("Running query : %s" % query)
         results = self.green_board_bucket.run_query(query)
         all_results.extend(results)
 
+        #query = "select s1.`build`,s1.name, s1.component, s1.failCount, s1.totalCount, s1.build_id, s1.url || tostring(s1.build_id) \
+        #        as full_url, 'more failures than {2}' as reason from {0} s1 left outer join {0} s2 on s1.name = s2.name\
+        #        and s2. `build` = '{2}' \
+        #        and lower(s2.os) = 'centos' and ( s2.url like '%test_suite_executor-jython/%' or s2.url like '%test_suite_executor-TAF/%' or s2.url like '%test_suite_executor/%') \
+        #        and s2.name not like 'centos-rqg%' where s1.`build` = '{1}' and lower(s1.os) = 'centos' \
+        #        and s1.result = 'UNSTABLE' and ( s1.url like '%test_suite_executor-jython/%' or s1.url like '%test_suite_executor-TAF/%' or s1.url like '%test_suite_executor/%') and s1.name not like 'centos-rqg%'\
+        #        and (s1.failCount - s2.failCount) > 0 order by s1.name".format(GREENBOARD_DB_BUCKETNAME,
+        #                                                                       current_build_num, prev_stable_build_num)
         query = "select s1.`build`,s1.name, s1.component, s1.failCount, s1.totalCount, s1.build_id, s1.url || tostring(s1.build_id) \
                 as full_url, 'more failures than {2}' as reason from {0} s1 left outer join {0} s2 on s1.name = s2.name\
                 and s2. `build` = '{2}' \
-                and lower(s2.os) = 'centos' and ( s2.url like '%test_suite_executor-jython/%' or s2.url like '%test_suite_executor-TAF/%' or s2.url like '%test_suite_executor/%') \
+                and lower(s2.os) = 'centos' and ( s2.url like '%test_suite_executor-jython/%' or s2.url like '%test_suite_executor/%') \
                 and s2.name not like 'centos-rqg%' where s1.`build` = '{1}' and lower(s1.os) = 'centos' \
-                and s1.result = 'UNSTABLE' and ( s1.url like '%test_suite_executor-jython/%' or s1.url like '%test_suite_executor-TAF/%' or s1.url like '%test_suite_executor/%') and s1.name not like 'centos-rqg%'\
+                and s1.result = 'UNSTABLE' and ( s1.url like '%test_suite_executor-jython/%' or s1.url like '%test_suite_executor/%') and s1.name not like 'centos-rqg%'\
                 and (s1.failCount - s2.failCount) > 0 order by s1.name".format(GREENBOARD_DB_BUCKETNAME,
                                                                                current_build_num, prev_stable_build_num)
+
 
         logger.info("Running query : %s" % query)
         results = self.green_board_bucket.run_query(query)
